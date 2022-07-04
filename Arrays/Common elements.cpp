9cfa1db6d13338d49a14c78aa0ed30f8d0e23e88
Antra Phukan <69572448+antrap@users.noreply.github.com>
2022-07-04 22:15:31 +0530

    Update Common elements.cpp

--- Arrays/Common elements.cpp
+++ Arrays/Common elements.cpp
@@ -1,16 +1,17 @@
-//works when all the elements are distinct
 vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
             //code here.
-            unordered_set<int> hash1;
+            unordered_map<int,bool> hash1;
             for(int i=0;i<n1;i++){
-                hash1.insert(A[i]);
+                hash1[A[i]]=true;
             }
             
             stack<int> s;
             for(int i=0;i<n2;i++){
-                if(hash1.find(B[i])!=hash1.end())
+                if(hash1.find(B[i])!=hash1.end() && hash1[B[i]]==true){
                 s.push(B[i]);//common of 1 and 2 are stored
+                hash1[B[i]]=false;
+                }
             }
             unordered_set<int> hash2;
             for(int i=0;i<n3;i++){
